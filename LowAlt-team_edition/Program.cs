4de882a2664f16f50b74315a22b96d6a1b721903
jovanni0 <cucea.ioan2004@gmail.com>
2025-01-15 19:39:41 +0200

    used Company class

--- LowAlt-team_edition/Program.cs
+++ LowAlt-team_edition/Program.cs
@@ -1,6 +1,4 @@
-﻿using LowAlt_team_edition.command_line;
-using LowAlt_team_edition.misc_classes;
-using LowAlt_team_edition.services;
+﻿using LowAlt_team_edition.misc_classes;
 
 namespace LowAlt_team_edition;
 
@@ -14,22 +12,7 @@ class Program
             RoutesFile = "data/routes.txt"
         };
 
-        /// read the data from the files
-        var dataLoader = new DataLoaderService(dataContext.FlightsFile, dataContext.RoutesFile);
-        (var routes, var flights) = dataLoader.GetData();
-        dataContext.Routes = routes;
-        dataContext.Flights = flights;
-
-        /// run the console application
-        var interfata = new UserInterface(dataContext);
-        interfata.StartInteraction();
-
-        /// save the data in the files
-        AccountWriterService accountsWriter = new AccountWriterService(dataContext.AccountsFile);
-        accountsWriter.WriteAccountsToFile(dataContext.Passengers);
-        FlightWriterService flightsWriter = new FlightWriterService(dataContext.FlightsFile);
-        flightsWriter.WriteFlightsToFile(dataContext.Flights);
-        RouteWriterService routeWriter = new RouteWriterService(dataContext.RoutesFile);
-        routeWriter.WriteRoutesToFile(dataContext.Routes);
+        Company company = new Company("MurderInc", dataContext);
+        company.Begin();
     }
 }
--- LowAlt-team_edition/misc_classes/Company.cs
+++ LowAlt-team_edition/misc_classes/Company.cs
@@ -1,3 +1,6 @@
+using LowAlt_team_edition.command_line;
+using LowAlt_team_edition.services;
+
 namespace LowAlt_team_edition.misc_classes;
 
 public class Company
@@ -17,10 +20,34 @@ public class Company
     /// </summary>
     public List<Ruta> Routes { get; set; }
 
-    public Company(string name, List<Flight> fleet, List<Ruta> routes)
+    public readonly DataContext Context;
+
+    public Company(string name, DataContext dataContext)
     {
         Name = name;
-        Fleet = fleet;
-        Routes = routes;
+        Context = dataContext;
+        Fleet = dataContext.Flights;
+        Routes = dataContext.Routes;
+    }
+
+    public void Begin()
+    {
+        /// read the data from the files
+        var dataLoader = new DataLoaderService(Context.FlightsFile, Context.RoutesFile);
+        (var routes, var flights) = dataLoader.GetData();
+        Context.Routes = routes;
+        Context.Flights = flights;
+
+        /// run the console application
+        var interfata = new UserInterface(Context);
+        interfata.StartInteraction();
+
+        /// save the data in the files
+        AccountWriterService accountsWriter = new AccountWriterService(Context.AccountsFile);
+        accountsWriter.WriteAccountsToFile(Context.Passengers);
+        FlightWriterService flightsWriter = new FlightWriterService(Context.FlightsFile);
+        flightsWriter.WriteFlightsToFile(Context.Flights);
+        RouteWriterService routeWriter = new RouteWriterService(Context.RoutesFile);
+        routeWriter.WriteRoutesToFile(Context.Routes);
     }
 }
